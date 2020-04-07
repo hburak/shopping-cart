a0cbd4168f0179678419f654183cb425cb944f46
halil burak cetinkaya <hburak3534@gmail.com>
2020-04-08 02:24:18 +0300

    shopping cart add item - added mutations, actions, getters

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <h1>Product List</h1>
-    <img v-if="loading" src="https://i.gifer.com/7plQ.gif" />
-    <ul v-else>
-      <li v-for="product in products" :key="product.id">{{ product.title }} - {{ product.price }}</li>
+    <p v-if="products.length <= 0">No products available</p>
+    <div v-else>
+      <h1>Product List</h1>
+      <img v-if="loading" src="https://i.gifer.com/7plQ.gif" />
+      <ul v-else>
+        <li v-for="product in products" :key="product.id">
+          {{ product.title }} - {{ product.price }} - {{ product.inventory }}
+          <button
+            @click="addToCart(product)"
+          >Add to cart</button>
+        </li>
+      </ul>
+    </div>
+    <h2>Cart</h2>
+    <ul>
+      <li v-for="item in cart" :key="item.id">{{ item.title }} - {{ item.quantity }}</li>
     </ul>
+    <p>Total cart price: {{ this.$store.getters.total }}</p>
   </div>
 </template>
 
@@ -13,12 +26,20 @@ export default {
   computed: {
     products() {
       return this.$store.getters.availableProducts;
+    },
+    cart() {
+      return this.$store.getters.cart;
     }
   },
   data() {
     return {
       loading: false
     };
+  },
+  methods: {
+    addToCart(product) {
+      this.$store.dispatch("addToCart", product);
+    }
   },
   created() {
     /* coupling to api removed from the component
--- src/store/index.js
+++ src/store/index.js
@@ -7,16 +7,40 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     products: [],
+    cart: [],
+    total: 0,
   },
   getters: {
     // eslint-disable-next-line no-unused-vars
     availableProducts(state, getters) {
       return state.products.filter((product) => product.inventory > 0);
     },
+    cart(state) {
+      return state.cart;
+    },
+    total(state) {
+      return state.total;
+    },
   },
   mutations: {
     setProducts(state, products) {
       this.state.products = products;
+    },
+    pushToCart(state, product) {
+      this.state.cart.push({
+        id: product.id,
+        title: product.title,
+        quantity: 1,
+      });
+    },
+    incrementItemCount(state, item) {
+      item.quantity++;
+    },
+    decrementInventory(state, product) {
+      product.inventory--;
+    },
+    incrementTotal(state, productPrice) {
+      this.state.total += productPrice;
     },
   },
   actions: {
@@ -30,6 +54,21 @@ export default new Vuex.Store({
           resolve();
         });
       });
+    },
+    addToCart(context, product) {
+      if (product.inventory > 0) {
+        const item = context.state.cart.find((item) => item.id === product.id);
+        if (!item) {
+          // push to cart
+          context.commit("pushToCart", product);
+        } else {
+          // increment the count
+          context.commit("incrementItemCount", item);
+        }
+        //commit
+        context.commit("incrementTotal", product.price);
+        context.commit("decrementInventory", product);
+      }
     },
   },
   modules: {},
