e7e876aa2c654f76dee9f231bc0cc76128832b9a
halil burak cetinkaya <hburak3534@gmail.com>
2020-04-08 00:15:54 +0300

    added mutations to the vuex store and used it

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,29 @@
 import Vue from "vue";
 import Vuex from "vuex";
+// eslint-disable-next-line no-unused-vars
+import shop from "@/api/shop.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    products: [],
+  },
+  getters: {
+    productsCount() {
+      return this.state.products.length;
+    },
+  },
+  mutations: {
+    setProducts(state, products) {
+      this.state.products = products;
+    },
+  },
+  actions: {
+    fetchProducts() {
+      // make the call
+      // run the setProducts mutation
+    },
+  },
+  modules: {},
 });
